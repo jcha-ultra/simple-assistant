bce40cfeca585126665c65ac4a1b3b025963ef15
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2023-06-18 08:35:08 -0400

    wrapup updates

--- simple_assistant/core.py
+++ simple_assistant/core.py
@@ -1,42 +1,58 @@
 """Core module for the simple assistant."""
 
+from functools import reduce
 from pathlib import Path
-from typing import Optional
+from typing import Dict, List, Optional, Tuple
 
 from yaml import dump, safe_load
+from dotenv import dotenv_values
 
 from .utilities import quick_import
-from .types import Message, MessageSequence
+from .types import Message, MessageSequence, MessageType
 
 
-def run_assistant(conversation: str, user_input: Optional[str] = None):
+def run_assistant(
+    config_file: str,
+    conversation: str,
+    user_input: Optional[str] = None,
+    write_conversation: bool = False,
+) -> List[Dict[str, str]]:
     """Run the assistant."""
-    with open("config.yml", encoding="utf-8") as config_file:
+    with open(Path(f"configs/{config_file}.yml"), encoding="utf-8") as config_file:
         config = safe_load(config_file)
     prompts: MessageSequence = quick_import(
         Path(f"prompts/{config['prompt']}.py")
     ).load_prompts()
-    conversation_path = Path(f"conversations/{conversation}.yml")
+    conversation_path = Path(config["conversation_dir"]) / f"{conversation}.yml"
     if not conversation_path.exists():
         conversation_path.touch()
         conversation_path.write_text("[]", encoding="utf-8")
-    with open(
-        f"conversations/{conversation}.yml", encoding="utf-8"
-    ) as conversation_file:
+    assistant_name = config["assistant_name"]
+
+    def map_from_assistant_name(name: str, content: str) -> Tuple[MessageType, str]:
+        if name == assistant_name:
+            return "assistant", content
+        if name == "user":
+            return "user", content
+        raise ValueError(
+            f"Unknown name {name} from conversation file. Must be {assistant_name} or user."
+        )
+
+    with open(conversation_path, encoding="utf-8") as conversation_file:
         conversation_history: MessageSequence = [
-            Message(*next(iter(message.items())))
+            Message(*map_from_assistant_name(*next(iter(message.items()))))
             for message in safe_load(conversation_file)
         ]
     # if user input was not provided, use the input from the last message
     if user_input is None:
         if not conversation_history or conversation_history[-1].type != "user":
             raise ValueError(
-                "No user input provided and no user input found in conversation file."
+                f'Conversation thread `{conversation_path}` must end with a user message. Please make sure that the last line of the yml file is from the user. Example: "- user: Tell me about yourself.")'
             )
         user_input = conversation_history[-1].content
         conversation_history = conversation_history[:-1]
     # if user input was provided, and input also exists in conversation history, then there's a conflict
-    elif conversation_history and conversation_history[-1].type == "user": 
+    elif conversation_history and conversation_history[-1].type == "user":
         raise ValueError(
             "User input provided, but user input already exists in conversation file."
         )
@@ -57,26 +73,42 @@ def run_assistant(conversation: str, user_input: Optional[str] = None):
         Message("user", user_input),
     ]
     messages = [message for message in messages if message is not None]
-    response: str = model.run(messages, config["model_name"], config["model_settings"])
+    environment_values = reduce(
+        lambda dict_1, dict_2: {**dict_1, **dict_2},
+        [dotenv_values(Path(env_file)) for env_file in config["environment_variables"]],
+    )
+    response: str = model.run(
+        messages, config["model_name"], config["model_args"], environment_values
+    )
     conversation_history = [
         *conversation_history,
         Message("user", user_input),
         Message("assistant", response),
     ]
+
+    def map_to_assistant_name(message_type: MessageType) -> str:
+        if message_type == "assistant":
+            return assistant_name
+        if message_type == "user":
+            return message_type
+        raise ValueError(
+            f"Cannot map message type `{message_type}`. Must be `assistant` or `user`."
+        )
+
     history_to_save = [
-        {message.type: message.content.strip()}
+        {map_to_assistant_name(message.type): message.content.strip()}
         for message in conversation_history
         if message.type != "system"
     ]
-
-    with open(
-        f"conversations/{conversation}.yml", mode="w", encoding="utf-8"
-    ) as conversation_file:
-        dump(
-            history_to_save,
-            conversation_file,
-            default_flow_style=False,
-            default_style="|",
-            allow_unicode=True,
-            indent=0,
-        )
+    if write_conversation:
+        with open(conversation_path, mode="w", encoding="utf-8") as conversation_file:
+            dump(
+                history_to_save,
+                conversation_file,
+                default_flow_style=False,
+                default_style="|",
+                allow_unicode=True,
+                indent=0,
+            )
+        print(f"Reply saved to \033[32m`{conversation_path.absolute()}`.\033[0m")
+    return history_to_save
